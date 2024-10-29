6cf3dc7284a403aee4ae314159f2a7f9db0792c7
yanglifeng <631676736@qq.com>
2024-10-29 18:13:30 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import print from './print.js';
 import delay from './delay.js';
 import printObj from './text.js';
 import './style.css';
+import logo from './logo.png';
 
 async function run() {
   for (let i = 0, len = printObj.text.length; i < len; i++) {
     print(printObj.text.slice(0, i));
     await delay(printObj.duration);
   }
+
+  var img = document.createElement('img');
+  img.src = logo;
+  document.body.appendChild(img)
 }
+
 run();
